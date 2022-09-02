b09bfe5f27814c36f89db4cdd932bf2b829c2d95
M-yoshimura-ML <masahiro.yoshimura@monstar-lab.com>
2022-09-02 22:58:11 +0800

    user api serializer & view & urls

---
+++ app/user/urls.py
@@ -0,0 +1,13 @@
+"""
+URL mapping for the user API.
+"""
+from django.urls import path
+
+from . import views
+
+
+app_name = 'user'
+
+url_patterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+]
