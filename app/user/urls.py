5039e5bdfe45dec23b3c84b5d0aad7db9b3219ef
M-yoshimura-ML <masahiro.yoshimura@monstar-lab.com>
2022-09-03 15:37:16 +0800

    fixed to urlpatterns

--- app/user/urls.py
+++ app/user/urls.py
@@ -8,7 +8,7 @@ from . import views
 
 app_name = 'user'
 
-url_patterns = [
+urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
     path('token/', views.CreateTokenView.as_view(), name='token'),
 ]
