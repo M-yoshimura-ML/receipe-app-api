9ce24e2b77a7ec1d6e6573c04709d859a104c8ca
M-yoshimura-ML <masahiro.yoshimura@monstar-lab.com>
2022-09-03 14:31:39 +0800

    added create token

--- app/app/settings.py
+++ app/app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'core',
     'user',
     'rest_framework',
+    'rest_framework.auth.token',
     'drf_spectacular',
 ]
 
--- app/user/urls.py
+++ app/user/urls.py
@@ -10,4 +10,5 @@ app_name = 'user'
 
 url_patterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token', views.CreateTokenView.as_view(), mame='token'),
 ]
