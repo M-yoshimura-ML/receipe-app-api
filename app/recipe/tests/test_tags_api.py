eaa5ba0fc510c2bb1ed4db028df4f29459180408
M-yoshimura-ML <masahiro.yoshimura@monstar-lab.com>
2022-09-05 19:13:21 +0800

    update tag & test

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -16,6 +16,11 @@ from ..serializers import TagSerializer
 TAG_URL = reverse('recipe:tag-list')
 
 
+def detail_url(tag_id):
+    """Create and return a tag detail url."""
+    return reverse('recipe:tag-detail', args=[tag_id])
+
+
 def create_user(email='user@example.com', password='testpass123'):
     """Create and return a user."""
     return get_user_model().object.create_user(email=email, password=password)
@@ -27,7 +32,7 @@ class PublicTagsAPIsTest(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-    def test_auth_required(self ):
+    def test_auth_required(self):
         """Test auth is required for retrieving tags."""
         res = self.client.get(TAG_URL)
 
@@ -66,3 +71,15 @@ class PrivateTagsAPIsTest(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
+
+    def test_update_tag(self):
+        """Test updating a tag."""
+        tag = Tag.objects.create(user=self.user, name='After Dinner')
+
+        payload = {'name': 'Dessert'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
